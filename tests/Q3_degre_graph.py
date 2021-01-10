6909d330d973484b98da6497c813708c6e01f449
Ruimy Benjamin Mac <ruimyb@gmail.com>
2021-01-10 20:51:53 +0000

    Test questions added

---
+++ tests/Q3_degre_graph.py
@@ -0,0 +1,10 @@
+#Q2:
+# On rajoute une méthode de classe qui permet à l'utilisateur de créer le graphe directement
+# depuis un fichier via. instantiate_graph_from_file
+import sys
+sys.path.append("../src/")
+from graphe import Graphe
+graph = Graphe.instantiate_graph_from_file("test_degree.txt")
+print(graph)
+deg = graph.degre("A")
+print("Degré de A est: {}".format(deg))
